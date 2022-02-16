3c6dad7f59e04cc046171987476de6199895f123
QISM1 <qism1@QISM1s-MacBook-Pro-2.local>
2022-02-16 10:21:53 -0400

    Vanilla splitting app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,13 +11,24 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    
+    @FocusState var isFocus: Bool
     let tipPercentages = [10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tip = Double(tipPercentage)
+        let tipAmount = ( checkAmount / 100 ) * tip
+        let grandTotal = checkAmount + tipAmount
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($isFocus)
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
@@ -26,10 +37,29 @@ struct ContentView: View {
                 }
                 
                 Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                } header: {
+                    Text("How much do you want to leave?")
+                }
+                
+                Section {
                     Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Done") {
+                        isFocus = false
+                    }
+                }
+            }
         }
     }
 }
